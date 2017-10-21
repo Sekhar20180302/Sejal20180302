b9e87116a0c194e9f551281e0a7bfdb701afd8b8
Sudhanshu Samal <samal.8757@gmail.com>
2017-10-22 01:35:04 +0530

    Test File to check

---
+++ Selenium/src/test/java/SeleniumTest/Selenium/Test.java
@@ -0,0 +1,22 @@
+package SeleniumTest.Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sekhar\\Eclipse-Workspace\\Selenium Drivers\\chromedriver.exe");
+		 
+		// Initialize browser
+		WebDriver driver=new ChromeDriver();
+		 
+		// Open facebook
+		driver.get("http://www.facebook.com");
+		 
+		// Maximize browser
+		driver.manage().window().maximize();
+}
+	}
