1ea0cf68ce73554c5a8962a457099e5dbc8edf1b
Sudhanshu Samal <samal.8757@gmail.com>
2018-04-01 02:40:52 +0530

    Three new files

--- ExplicitWait/src/explicitPackage/TheBankJob.java
+++ ExplicitWait/src/explicitPackage/TheBankJob.java
@@ -1,13 +1,13 @@
 package explicitPackage;
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
-public class TheBankJob {	
+public class TheBankJob {
+	@Test
 	public void BankJob(){
 		// TODO Auto-generated method stub
 		String expectedTitle="Guru99 Bank Home Page";
